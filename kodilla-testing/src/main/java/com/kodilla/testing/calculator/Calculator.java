773229ebf20b8eebacda18749a7c68d5af5b6f8e
Anna Pawlas <annapawlas1@gmail.com>
2022-12-09 22:51:14 +0100

    6.2 Testing simple calculator app

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+    public double substractBFromA(double a, double b) {
+        return a - b;
+    }
+
+}
