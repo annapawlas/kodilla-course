8f8798c284085164509b8c724a824b233846b6a4
Anna Pawlas <annapawlas1@gmail.com>
2023-03-03 20:29:11 +0100

    14. Poprawiony plik - dodanie settings.gradle

---
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpringWebApplication.class, args);
+    }
+
+}
