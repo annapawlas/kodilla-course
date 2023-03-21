ac1f958defa08827570117af1a9e4ebc80942b3e
Anna Pawlas <annapawlas1@gmail.com>
2023-03-21 23:55:17 +0100

    17.1. Zadanie: Dołączamy framework Hibernate.

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
